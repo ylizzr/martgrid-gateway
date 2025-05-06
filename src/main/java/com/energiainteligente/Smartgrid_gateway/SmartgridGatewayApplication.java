dbbb9162735419029941605b055a905a18443591
ylizzr <ylizarazor@ucentral.edu.co>
2025-05-06 18:49:42 -0500

    Estructura

---
+++ src/main/java/com/energiainteligente/Smartgrid_gateway/SmartgridGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.energiainteligente.Smartgrid_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartgridGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartgridGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/energiainteligente/Smartgrid_gateway/SmartgridGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energiainteligente.Smartgrid_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartgridGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
